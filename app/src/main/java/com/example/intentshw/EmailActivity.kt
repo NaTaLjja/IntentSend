b8401c1259e2133b6c950750d58c6734281d7a01
Natalja <natali.dorogaya@gmail.com>
2023-08-24 06:18:52 +0200

    Added EmailActivity and  email_layout

---
+++ app/src/main/java/com/example/intentshw/EmailActivity.kt
@@ -0,0 +1,10 @@
+package com.example.intentshw
+
+import android.app.Activity
+import android.os.Bundle
+
+class EmailActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
